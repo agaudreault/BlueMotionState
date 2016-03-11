3e50a98272142fc81f662d1d327a287ea5f24cf6
Alexandre Gaudreault <gaua2616@usherbrooke.ca>
2016-03-11 14:22:10 -0500

    stream working from 4 to 5

--- BMSPrototype/app/src/main/java/bms/bmsprototype/socket/SocketInputRunnable.java
+++ BMSPrototype/app/src/main/java/bms/bmsprototype/socket/SocketInputRunnable.java
@@ -1,11 +1,15 @@
 package bms.bmsprototype.socket;
 
+import android.inputmethodservice.InputMethodService;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Arrays;
 
 import bms.bmsprototype.WifiDirectActivity;
 
@@ -29,13 +33,20 @@ public class SocketInputRunnable implements Runnable {
             return;
 
         try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
+            //BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
+            InputStream is = _socket.getInputStream();
+            byte[] buf = new byte[8192];
+            int len;
 
             while(true) {
-                String message = bufferedReader.readLine();
+                while ((len = is.read(buf, 0, buf.length)) != -1) {
+
+                //String message = bufferedReader.readLine();
+                String message = new String(buf, 0, len);
 
                 if(message != null)
                     _activity.onMessageReceived(message);
+            }
             }
         } catch (IOException e) {
             e.printStackTrace();
