1c3410937279b5c60596baa38c92c1337f71d286
unknown <cara1912@DINF-0014-45A.DInf.FSCI.USherbrooke.ca>
2016-04-10 16:51:30 -0400

    Added javadoc in helper package

--- BMSPrototype/app/src/main/java/bms/bmsprototype/helper/PermissionHelper.java
+++ BMSPrototype/app/src/main/java/bms/bmsprototype/helper/PermissionHelper.java
@@ -1,42 +1,12 @@
 package bms.bmsprototype.helper;
 
-import android.app.Fragment;
 import android.content.pm.PackageManager;
-import android.support.v13.app.FragmentCompat;
 import android.util.Log;
-
-import bms.bmsprototype.dialog.ConfirmationDialog;
 
 /**
  * Created by gaua2616 on 2016-02-02.
  */
 public class PermissionHelper {
-
-    /**
-     * Gets whether you should show UI with rationale for requesting permissions.
-     *
-     * @param permissions The permissions your app wants to request.
-     * @return Whether you can show permission rationale UI.
-     */
-    private static boolean shouldShowRequestPermissionRationale(Fragment frag, String[] permissions) {
-        for (String permission : permissions) {
-            if (FragmentCompat.shouldShowRequestPermissionRationale(frag, permission)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Requests permissions needed for recording video.
-     */
-    public static void requestVideoPermissions(Fragment frag, int req, String[] perms, String dialog) {
-        if (shouldShowRequestPermissionRationale(frag, perms)) {
-            ConfirmationDialog.newInstance(perms,req).show(frag.getChildFragmentManager(), dialog);
-        } else {
-            FragmentCompat.requestPermissions(frag, perms, req);
-        }
-    }
 
     /**
      * Check the permission result
--- BMSPrototype/app/src/main/java/bms/bmsprototype/helper/WifiDirectHelper.java
+++ BMSPrototype/app/src/main/java/bms/bmsprototype/helper/WifiDirectHelper.java
@@ -1,24 +1,7 @@
-/*
- * Copyright (C) 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package bms.bmsprototype.helper;
 
 import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
-import android.os.AsyncTask;
 
 import java.lang.reflect.Method;
 import java.net.Socket;
@@ -31,6 +14,11 @@ public class WifiDirectHelper {
 
     private static final String TAG = "WifiDirectHelper";
 
+    /**
+     * Delete all persistent groups on the device
+     * @param manager
+     * @param channel
+     */
     public static void deletePersistentGroups(WifiP2pManager manager, WifiP2pManager.Channel channel) {
         try {
             Method[] methods = WifiP2pManager.class.getMethods();
@@ -47,6 +35,14 @@ public class WifiDirectHelper {
         }
     }
 
+    /**
+     * Open a socket on the specified port with the Wifi Direct group info.
+     * If the device is owner of the group, it starts a ServerSocket, a Socket otherwise.
+     * @param info Informations about the group of the device
+     * @param port Port to use for the socket connection
+     * @param listener Listener to call when the socket connection throws events
+     * @return The task created to open the socket connection
+     */
     public static SocketTask openSocketConnection(WifiP2pInfo info, int port, SocketTask.WifiDirectSocketEventListener listener) {
         if (!info.groupFormed)
             return null;
@@ -62,6 +58,11 @@ public class WifiDirectHelper {
         return task;
     }
 
+    /**
+     * Checks if a socket in non-null and connected
+     * @param socket The socket to validate
+     * @return If the socket is valid or not
+     */
     public static boolean isSocketValid(Socket socket) {
         return socket != null && socket.isConnected();
     }
